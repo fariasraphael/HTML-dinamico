0373b51898d89f1179dd5c4edd6ccf56a5a2f3c5
fariasraphael <farias.raphael@gmail.com>
2023-04-07 17:53:14 -0300

    adiciono modificações no exercicio que converte anos para dias

--- exercicio-ConversosAnosPraDias/main.js
+++ exercicio-ConversosAnosPraDias/main.js
@@ -5,7 +5,7 @@ document.getElementsByTagName("button")[0].onclick = (e) => {
     let dias = anos * 365;
 
     let p = document.createElement('p');
-    
+
     p.textContent = `${anos} ano(s) equivalem a ${dias} dias`;
 
     document.body.appendChild(p);
