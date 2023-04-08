60a79f2fef8abacbdf300d8ab76f9989b26fdc01
fariasraphael <farias.raphael@gmail.com>
2023-04-08 09:24:56 -0300

    envio exercicio final da aula

--- exercicio-7/main.js
+++ exercicio-7/main.js
@@ -1,6 +1,26 @@
-function estaEntre(numero, minimo, maximo, inclusivo){
-    if(inclusivo) {
-        return (number >= minimo && numero <= maximo)
+function estaEntre(numero, minimo, maximo, inclusivo) {
+
+    if (inclusivo) {
+        return (numero >= minimo && numero <= maximo)
     }
-    return (number > minimo && numero < maximo)
+    return (numero > minimo && numero < maximo)
+}
+
+
+
+function verifica() {
+    let number = Number(document.getElementById("num1").value);
+    let min = Number(document.getElementById("num2").value);
+    let max = Number(document.getElementById("num3").value);
+    let check = Boolean(document.getElementById("inclusivo").checked);
+
+    if (min > max) {
+        let buf = max; 
+        max = min;
+        min = buf;
+    }
+    let aux = estaEntre(number, min, max, check);
+    let p = document.createElement('p');
+    p.textContent = aux;
+    document.body.appendChild(p);
 }
