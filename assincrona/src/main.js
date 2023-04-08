f562146ba32ab7e6753c20a50294b7476c3b79d2
fariasraphael <farias.raphael@gmail.com>
2023-04-08 10:14:59 -0300

    adiciono exemplos do professor

--- assincrona/src/main.js
+++ assincrona/src/main.js
@@ -1,5 +1,21 @@
-function getData () {
-    console.log("entrou na função");
-    setTimeout (()=> {console.log("entrou no setTimeout()");}, 2000);
-    console.log("saiu da função");
+class User{
+    name;
+    age;
+    weight;
+}
+
+function getData (callback1) {
+    setTimeout ( () => {
+        callback1 ("callback 1");
+    }, 2000); 
+}
+
+function callback1(callback2) {
+    setTimeout ( () => {
+        callback2 ("callback 2");
+    }, 2000);
+}
+
+function callback2 (aux){
+    console.log(aux);
 }
