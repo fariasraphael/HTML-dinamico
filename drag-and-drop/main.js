1825da2a692067bbdf426ee1b39d8c7bfa49227a
fariasraphael <farias.raphael@gmail.com>
2023-04-15 18:20:51 -0300

    finalizo aula de drag and drop

--- drag-and-drop/main.js
+++ drag-and-drop/main.js
@@ -1,20 +1,40 @@
 let itens = document.querySelectorAll("[wm-dropzone] .item")
 
-
 let right = document.querySelector(".right");
 let left = document.querySelector(".left");
-console.log(right, left);
-console.log(itens);
 
 itens.forEach((item, index) => {
     if (item instanceof HTMLDivElement) {
         item.draggable = true;
         item.id = item.id || `draggable-item${index}`;
 
-        item.ondragstart = e =>{
-            e.dataTransfer.setData("")
+        item.ondragstart = e => {
+            e.dataTransfer.setData("item-id", e.target.id);
+            if (item.parentElement === right) {
+                left.classList.add("avaliable-to-drop");
+                right.classList.remove("avaliable-to-drop");
+            } else {
+                right.classList.add("avaliable-to-drop");
+                left.classList.remove("avaliable-to-drop");
+            }
         }
     }
 });
 
-/* 2:20:00 */
+let dropzones = document.querySelectorAll("[wm-dropzone]");
+
+dropzones.forEach((dropzone) => {
+    if (dropzone instanceof HTMLDivElement) {
+        dropzone.ondragover = (e) => e.preventDefault();
+        dropzone.ondrop = (e) => {
+            let id = e.dataTransfer.getData("item-id");
+            let item = document.getElementById(id);
+
+            e.target.appendChild(item)
+            //dropzone.appendChild(item);
+            left.classList.remove("avaliable-to-drop");
+            right.classList.remove("avaliable-to-drop");
+        };
+    }
+});
+
