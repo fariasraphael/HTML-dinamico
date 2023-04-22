f543fe9bf75c1be33e0a46d2de1428a1e03e000f
fariasraphael <farias.raphael@gmail.com>
2023-04-22 19:57:08 -0300

    agora foi tudo

--- aula2-lista-de-tarefas-desafio/main.js
+++ aula2-lista-de-tarefas-desafio/main.js
@@ -2,9 +2,37 @@ let form = document.getElementsByTagName("form")[0];
 
 form.addEventListener("submit", createTask);
 
-function createTask(evt){
+let ul = document.getElementsByTagName("ul")[0];
+
+
+function createTask(evt) {
     evt.preventDefault();
 
-    let
+    console.log(evt);
+
+
+    let title = document.getElementById("title").value;
+    let description = document.getElementById("description").value;
+
+
+    let li = document.createElement("li");
+    li.innerHTML = `
+    <div>
+        <h3> ${title} </h3>
+        <p> ${description} </p>
+    </div>
+    <input type="checkbox" onclick="removeTask(event)"/>
+    `;
+
+    ul.appendChild(li);
+
+    form.reset();
 }
-// 2:07
+
+function removeTask(evt) {
+    console.log(evt);
+    //ul.removeChild(evt.srcElement.parentElement);
+    //evt.srcElement.disabled = true;
+    evt.srcElement.setAttribute("disabled", true);
+    evt.srcElement.parentElement.classList = "task-finished"
+}
